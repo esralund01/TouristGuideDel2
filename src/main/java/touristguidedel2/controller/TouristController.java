ea9143f107e38841f6e154ec25245016f9ced451
zako0001 <zako0001@stud.kea.dk>
2024-09-16 14:28:53 +0200

    mandag

--- src/main/java/touristguidedel2/controller/TouristController.java
+++ src/main/java/touristguidedel2/controller/TouristController.java
@@ -1,4 +1,58 @@
 package touristguidedel2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import touristguidedel2.service.TouristService;
+
+@Controller
 public class TouristController {
+
+    private TouristService touristService;
+
+    public TouristController() {
+        touristService = new TouristService();
+    }
+
+    @GetMapping("/attractions")
+    public String attractions(Model model) {
+        model.addAttribute("touristAttractions",touristService.getAllAttractions());
+        return "attractionList";
+    }
+
+    @GetMapping("/attractions/{name}")
+    public String attractions1() {
+        return null;
+    }
+
+    @GetMapping("/attractions/{name}/tags")
+    public String attractions2() {
+        return null;
+    }
+
+    @GetMapping("/attractions/add")
+    public String attractions3() {
+        return null;
+    }
+
+    @PostMapping("/attractions/save")
+    public String attractions4() {
+        return null;
+    }
+
+    @GetMapping("/attractions/{name}/edit")
+    public String attractions5() {
+        return null;
+    }
+
+    @PostMapping("/attractions/update")
+    public String attractions6() {
+        return null;
+    }
+
+    @PostMapping("/attractions/delete/{name}")
+    public String attractions7() {
+        return null;
+    }
 }
--- src/main/java/touristguidedel2/model/TouristAttraction.java
+++ src/main/java/touristguidedel2/model/TouristAttraction.java
@@ -1,4 +1,23 @@
 package touristguidedel2.model;
 
 public class TouristAttraction {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
