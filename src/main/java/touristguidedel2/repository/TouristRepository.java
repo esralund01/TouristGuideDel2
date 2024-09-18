2f47b54b7060b1cabb45eedd41f9a767565a6023
esralund01 <calu0001.stud.kea.dk>
2024-09-18 11:24:53 +0200

    trin 6

--- src/main/java/touristguidedel2/repository/TouristRepository.java
+++ src/main/java/touristguidedel2/repository/TouristRepository.java
@@ -12,10 +12,10 @@ public class TouristRepository {
     public TouristRepository() {
 
         touristAttractions = new ArrayList<>(List.of(
-            new TouristAttraction("SMK","Museum for Kunst"),
-            new TouristAttraction("Odense Zoo","Europas bedste zoo"),
-            new TouristAttraction("Dyrehaven","Naturpark med skovområder"),
-            new TouristAttraction("Tivoli","Forlystelsespark i København centrum")
+            new TouristAttraction("SMK","Museum for Kunst","København",List.of("Musem", "Kunst")),
+            new TouristAttraction("Odense Zoo","Europas bedste zoo","Odense", List.of("Børnevenlig")),
+            new TouristAttraction("Dyrehaven","Naturpark med skovområder","Kongens Lyngby",List.of("Natur", "Gratis")),
+            new TouristAttraction("Tivoli","Forlystelsespark i København centrum","København",List.of("Børnevenlig"))
         ));
     }
 
